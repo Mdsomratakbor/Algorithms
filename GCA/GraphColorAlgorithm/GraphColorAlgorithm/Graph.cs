01ac07153fe4ce32f5d02c5db4273cb5a45dcdf8
MdSomratAkbor <somrat876@gmail.com>
2023-05-11 14:12:20 +0600

    graph color algorithm implement

---
+++ GCA/GraphColorAlgorithm/GraphColorAlgorithm/Graph.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphColorAlgorithm
+{
+    public class Graph
+    {
+       
+        int V = 0;
+        int[,] graph = new int[V, V] {};
+        public Graph(int vertices)
+        {
+            
+        }
+    }
+}
